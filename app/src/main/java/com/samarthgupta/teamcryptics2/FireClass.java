48871669b1a07fb1c37bfaa963f92bda57ef25f7
Samarth Gupta <samarthgupta1011@gmail.com>
2017-02-04 02:16:15 +0530

    Sam commit

---
+++ app/src/main/java/com/samarthgupta/teamcryptics2/FireClass.java
@@ -0,0 +1,101 @@
+package com.samarthgupta.teamcryptics2;
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by samarthgupta on 04/02/17.
+ */
+
+    public class FireClass extends MainActivity{
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference ref = firebaseDatabase.getReference();
+
+    public FireClass(){
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dsp : dataSnapshot.getChildren()){
+                    Log.i("TAG",dsp.getKey().toString());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+
+    public FireClass(final String where){
+
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    for (DataSnapshot dsp : dataSnapshot.child(where).getChildren()){
+                        Log.i("TAG",dsp.getKey().toString());
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
+        }
+
+    public FireClass(final String where1, final String where2){
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dsp : dataSnapshot.child(where1).child(where2).getChildren()){
+                    Log.i("TAG",dsp.getKey().toString());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+    }
+
+    void getVal(final String where1, final String where2, final String where3){
+        //THIS GIVES THE VALUE
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String val = dataSnapshot.child(where1).child(where2).child(where3).getValue().toString();
+                Log.i("TAG",val);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+    }
+
+
+
+    }
+
+
--- app/src/main/java/com/samarthgupta/teamcryptics2/MainActivity.java
+++ app/src/main/java/com/samarthgupta/teamcryptics2/MainActivity.java
@@ -2,6 +2,7 @@ package com.samarthgupta.teamcryptics2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -17,33 +18,71 @@ public class MainActivity extends AppCompatActivity {
 
         ImageButton ib1;
         EditText et1;
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference dref = firebaseDatabase.getReference();
 
-        @Override
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
             et1=(EditText)findViewById(R.id.editText);
             ib1=(ImageButton)findViewById(R.id.imageButton);
+            click();
 
-            ib1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    dref.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-                }
-            });
     }
+
+
+    void click(){
+
+        ib1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String t=et1.getText().toString();
+                goToTop(t);
+                Log.i("TAG","1st click");
+                if (t.matches("1")){
+                    new FireClass(); //Calling constructor
+                }
+                ib1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final String t1=et1.getText().toString();
+                        goToTop(t1);
+                        new FireClass(t1);   //Calling constructor
+                        Log.i("TAG","2st click");
+
+                        ib1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                final String t2=et1.getText().toString();
+                                goToTop(t2);
+                                Log.i("TAG","3st click");
+                                final FireClass fireClass1 = new FireClass(t1,t2); //Calling constructor
+
+                            ib1.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    String t3= et1.getText().toString();
+                                    goToTop(t3);
+                                    Log.i("TAG","4st click");
+                                    fireClass1.getVal(t1,t2,t3); //Calling function
+                                }
+                            });
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    } //End of click function
+
+    //Function for going to the top of click
+    void goToTop(String x){
+        if (x.matches("X")){
+            click();
+        }
+    }
+
+
 }
+
