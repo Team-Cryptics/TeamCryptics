26d6427588213c4a5276d505a47a7b9e62e135ef
VidurWadhwa <vidur2wadhwa@gmail.com>
2017-02-05 13:49:14 -0600

    First Commit

--- app/src/main/java/com/samarthgupta/teamcryptics2/FireClass.java
+++ app/src/main/java/com/samarthgupta/teamcryptics2/FireClass.java
@@ -1,12 +1,16 @@
 package com.samarthgupta.teamcryptics2;
 
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by samarthgupta on 04/02/17.
@@ -16,24 +20,30 @@ import com.google.firebase.database.ValueEventListener;
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference ref = firebaseDatabase.getReference();
+    ArrayList<String> myList = new ArrayList<String>();
 
 
-    public FireClass(final String where) {
+
+
+    public ArrayList<String> getList(final String where) {
 
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for (DataSnapshot dsp : dataSnapshot.child(where).getChildren()) {
+                    myList.add(dsp.getKey().toString());
                     Log.i("TAG", dsp.getKey().toString());
-                }
 
+                }
+                Log.i("TAG" , myList.get(0));
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
         });
+        return myList;
 
     }
 }
